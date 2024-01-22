aa873109737964842ca9790b3b3d980efb4a3814
giselle <yajisk0@gmail.com>
2024-01-22 16:42:51 +0000

    feat: create a comic list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,15 +27,15 @@ class HomeScreen extends StatelessWidget {
         future: webcomics,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webcomic = snapshot.data![index];
-                return Text(webcomic.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                height: 12,
-              ),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                ),
+              ],
             );
           }
           return const Center(
@@ -44,5 +44,57 @@ class HomeScreen extends StatelessWidget {
         },
       ),
     );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebComicModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        var webcomic = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    )
+                  ]),
+              width: 250,
+              child: Image.network(
+                webcomic.thumb,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              webcomic.title,
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
+      ),
+    );
+  }
+}
+
+Widget checkImage(String url) {
+  try {
+    return Image.network(url);
+  } catch (e) {
+    return const Icon(Icons.image);
   }
 }
