6603f327ae888c2c4cade7ca50aa5b3a6bf454ee
giselle <yajisk0@gmail.com>
2024-01-22 18:26:38 +0000

    feat: take out episode widget and add navigation using url_launcher

--- lib/widgets/detail_screen.dart
+++ lib/widgets/detail_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webcomic/models/webcomic_detail_model.dart';
 import 'package:webcomic/models/webcomic_episode_model.dart';
 import 'package:webcomic/services/api_service.dart';
+import 'package:webcomic/widgets/episode_widget.dart';
 
 class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
@@ -111,36 +114,7 @@ class _DetailScreenState extends State<DetailScreen> {
                         for (var episode in snapshot.data!.length > 10
                             ? snapshot.data!.sublist(0, 10)
                             : snapshot.data!)
-                          Container(
-                            margin: const EdgeInsets.only(
-                              bottom: 10,
-                            ),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              color: Colors.green.shade300,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 20),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    episode.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  const Icon(
-                                    Icons.chevron_right_rounded,
-                                    color: Colors.white,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          )
+                          Episode(episode: episode, webcomicId: widget.id)
                       ],
                     ); //using Column instead of ListView or ListViewBuilder is that we know the episodes.length is always going to be 10. Usually we need ListView when we want to optimise the list.
                   }
