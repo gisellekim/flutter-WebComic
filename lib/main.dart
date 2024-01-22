5fbfbf1c8464803c68b592392e1ed9441b5d7901
giselle <yajisk0@gmail.com>
2024-01-22 03:03:27 +0000

    chore: initial setup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Web Comic',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Container(),
+    );
+  }
+}
